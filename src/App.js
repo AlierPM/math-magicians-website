227e783afeedf2be5a0116e7b3ad1ade4e3d9351
Alier Philip <alierditmaguet@gmail.com>
2022-09-16 12:55:15 +0200

    Add Calculator.js to Components folder
    	modified .gitignore
      Add new files to src folder
    	- App.css
    	- App.js
    	- App.test.js
    	new file:   src/components/Calculator.js
    	modified:   src/index.js
    	deleted:    src/reportWebVitals.js

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+import Calculator from './components/Calculator';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return <Calculator />;
+  }
+}
+
+export default App;
