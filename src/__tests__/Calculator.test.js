a66bae19d225031707bab97c6ece77881c9cf37b
Alier Philip <alierditmaguet@gmail.com>
2022-09-23 17:10:01 +0200

    Add Calculator component test

---
+++ src/__tests__/Calculator.test.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import TestRenderer from 'react-test-renderer';
+import '@testing-library/jest-dom';
+import Calculator from '../components/Calculator';
+
+test('Unit Test Check For Calculator Component', () => {
+  render(<Calculator />);
+  const linkElement = screen.getByText(/Let us do some math/i);
+  expect(linkElement).toBeInTheDocument();
+});
+
+describe('Unit Test For Calculator component', () => {
+  it('Calculator Component renders correctly', () => {
+    const tree = TestRenderer.create(<Calculator />).toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+});
